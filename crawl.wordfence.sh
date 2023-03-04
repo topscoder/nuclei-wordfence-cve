dfe9a10d04d013300c8a379b4723d63dcfaa04a0
topscoder <86197446+topscoder@users.noreply.github.com>
2023-03-04 14:28:02 +0100

    wordfence crawler

---
+++ crawl.wordfence.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "CRAWLING" | lolcat
+
+katana -u https://wordfence.com/threat-intel/vulnerabilities/ -depth 10 | tee lib/sources/wordfence.com.crawler.txt
+
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-core/ | unfurl format %s://%d%p | sort | uniq | tee lib/sources/wordfence.com.wordpress-core.txt
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-themes/ | unfurl format %s://%d%p | sort | uniq | tee lib/sources/wordfence.com.wordpress-themes.txt
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | tee lib/sources/wordfence.com.wordpress-plugins.txt
+
+# Show only new URLs
+echo "\n NEW VULNERABILITIES FOR PLUGINS:\n"
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-plugins.txt
+#cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-plugins.txt > lib/sources/wordfence.com.wordpress-plugins.new.txt
+
+echo "\n NEW VULNERABILITIES FOR THEMES:\n"
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-themes/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-themes.txt
+#cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-themes/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-themes.txt > lib/sources/wordfence.com.wordpress-themes.new.txt
+
+echo "\n NEW VULNERABILITIES FOR CORE:\n"
+cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-core/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-core.txt
+#cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-core/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-core.txt > lib/sources/wordfence.com.wordpress-core.new.txt
