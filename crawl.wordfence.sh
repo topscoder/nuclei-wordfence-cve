f8d6e230ab268a8f4aeef1920726ec1bcec2ebaf
topscoder <86197446+topscoder@users.noreply.github.com>
2023-03-06 09:13:13 +0100

    Schmuck

--- crawl.wordfence.sh
+++ crawl.wordfence.sh
@@ -8,14 +8,14 @@ cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vuln
 cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | tee lib/sources/wordfence.com.wordpress-plugins.txt
 
 # Show only new URLs
-echo "\n NEW VULNERABILITIES FOR PLUGINS:\n"
+echo "\n\n NEW VULNERABILITIES FOR PLUGINS:\n" | lolcat
 cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-plugins.txt
 #cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-plugins/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-plugins.txt > lib/sources/wordfence.com.wordpress-plugins.new.txt
 
-echo "\n NEW VULNERABILITIES FOR THEMES:\n"
+echo "\n\n NEW VULNERABILITIES FOR THEMES:\n" | lolcat
 cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-themes/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-themes.txt
 #cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-themes/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-themes.txt > lib/sources/wordfence.com.wordpress-themes.new.txt
 
-echo "\n NEW VULNERABILITIES FOR CORE:\n"
+echo "\n\n NEW VULNERABILITIES FOR CORE:\n" | lolcat
 cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-core/ | unfurl format %s://%d%p | sort | uniq | anew lib/sources/wordfence.com.wordpress-core.txt
 #cat lib/sources/wordfence.com.crawler.txt | grep wordfence.com/threat-intel/vulnerabilities/wordpress-core/ | unfurl format %s://%d%p | sort | uniq | anew wordfence.com.wordpress-core.txt > lib/sources/wordfence.com.wordpress-core.new.txt
