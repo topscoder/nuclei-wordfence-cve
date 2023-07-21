90bd00cd182faa8870cdd1989668e4c1ee3f4cda
topscoder <86197446+topscoder@users.noreply.github.com>
2023-07-21 14:57:29 +0200

    Fixing template parsing errors

--- lib/parsers/WordfenceAPIParser.py
+++ lib/parsers/WordfenceAPIParser.py
@@ -11,11 +11,11 @@ from lib.parsers.ParserInterface import ParserInterface
 
 
 class WordfenceAPIParser(ParserInterface):
-    
+
     url = None
     html_content = None
 
-    def run(self, url, overwrite = False, force = False, overwrite_enhanced = False) -> bool:
+    def run(self, url, overwrite=False, force=False, overwrite_enhanced=False) -> bool:
         """Execute the Wordfence API Parser.
 
         Args:
@@ -32,7 +32,7 @@ class WordfenceAPIParser(ParserInterface):
             file_path = "./vulnerabilities.production.json"
             with open(file_path, "r") as file:
                 vulnerabilities = json.load(file)
-                
+
                 # Print the vulnerabilities
                 for vulnerability_id, vulnerability in vulnerabilities.items():
                     self.process_item(vulnerability, overwrite, force, overwrite_enhanced)
@@ -50,33 +50,34 @@ class WordfenceAPIParser(ParserInterface):
             if response.status_code == 200:
                 # The request was successful, so parse the JSON response
                 vulnerabilities = response.json()
-        
+
                 # Print the vulnerabilities
                 for vulnerability_id, vulnerability in vulnerabilities.items():
                     self.process_item(vulnerability, overwrite, force, overwrite_enhanced)
-            
+
             if response.status_code > 400:
                 logger.warning(red(f"[*] [HTTP {response.status_code}] Could not read URL: ${url}$"))
                 return False
-        
+
     def process_item(self, json_object, overwrite, force, overwrite_enhanced):
         title = json_object.get('title')
         description = json_object.get('description')
         cve_id = json_object.get('cve')
         if cve_id is None:
             cve_id = ""
-        
+
         software = json_object.get('software', [])
         if software:
             for item in software:
                 software_type = item.get('type')
                 software_slug = item.get('slug')
-        
-                object_category_slug = "plugins" if software_type == 'plugin' else 'unknown'
+
+                object_category_slug = "plugins" \
+                    if software_type == 'plugin' else 'unknown'
 
                 template_id = self.get_template_id(cve_id, item)
 
-                target_filename = template_id + ".yaml" 
+                target_filename = template_id + ".yaml"
                 logger.info(f"[ ] Target filename: {target_filename}")
 
                 # determine file path
@@ -105,9 +106,8 @@ class WordfenceAPIParser(ParserInterface):
                         logger.info(yellow(f"[*] https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/main/cves/{year}/{target_filename}"))
                         logger.info(yellow(f"[*] Skipping. Use --force if you want to ignore this and create a new template."))
                         return False
-                
-                 # Manually enhanced templates can be marked with "# Enhanced" in last line of the template.
-                
+
+                # Manually enhanced templates can be marked with "# Enhanced" in last line of the template.
                 # This ensures the template is overwritten only after using the --overwrite-enhanced flag.
                 if os.path.isfile(f"{filepath}"):
                     with open(f"{filepath}", "r") as fp:
@@ -117,7 +117,7 @@ class WordfenceAPIParser(ParserInterface):
                                 logger.info(yellow(f"[*] Note: There is already an **enhanced** template in our local nuclei-templates repo: {filepath}"))
                                 logger.info(yellow(f"[*] Skipping. Use --overwrite-enhanced if you want to ignore this and overwrite the template."))
                                 return False
-                            
+
                 if self.type_is_supported(software_type) is False:
                     logger.warning(red(f"[*] Skipping. Software type {software_type} is not supported."))
                     # TODO could be a page with a vuln which affects more plugins/themes
@@ -137,7 +137,7 @@ class WordfenceAPIParser(ParserInterface):
                     cvss_rating = self.determine_severity(title)
 
                 reference_list = json_object.get('references', {})
-                
+
                 object_category_tag = self.get_object_category_tag(software_type)
                 find_file = self.target_version_file(software_type, item)
                 version_regex = self.get_version_regex(software_type)
@@ -145,9 +145,13 @@ class WordfenceAPIParser(ParserInterface):
                 affected_versions = item.get('affected_versions')
                 for version_range, version_data in affected_versions.items():
                     affected_version = self.get_affected_version(version_data)
-                
+
                 # Parse template
-                tpl = self.get_template_filename(software_type)
+                if len(reference_list) > 0:
+                    tpl = self.get_template_filename(software_type)
+                else:
+                    tpl = self.get_template_filename(software_type, False)
+
                 with open(tpl) as template:
                     template_content = template.read()
                     template_content = template_content.replace('__TEMPLATE_ID__', str(template_id))
@@ -171,13 +175,12 @@ class WordfenceAPIParser(ParserInterface):
                     with open(filepath, 'w+') as target:
                         target.write(template_content)
                         logger.info(green("[>] " + filepath))
-                
         else:
-            logger.warning(f"nothing to do")
-    
+            logger.warning("nothing to do")
+
     def determine_severity(self, title) -> str:
         # Severity scores
-        SEVERITY_LOW = 1
+        # SEVERITY_LOW = 1
         SEVERITY_MEDIUM = 2
         SEVERITY_HIGH = 3
         SEVERITY_CRITICAL = 4
@@ -189,25 +192,25 @@ class WordfenceAPIParser(ParserInterface):
 
         if "File Inclusion" in title:
             score = SEVERITY_CRITICAL
-        
+
         if "SQL Injection" in title:
             score = SEVERITY_CRITICAL
-        
+
         if "Unauthenticated PHP Object Injection" in title:
             score = SEVERITY_CRITICAL
 
         if "Remote Code Execution" in title:
             score = SEVERITY_CRITICAL
-        
+
         if "Authentication Bypass" in title:
             score = SEVERITY_CRITICAL
 
         if "Cross-Site Scripting" in title:
             score = SEVERITY_HIGH
-        
+
         if "Authorization Bypass" in title:
             score = SEVERITY_HIGH
-        
+
         if "Missing Authorization" in title:
             score = SEVERITY_HIGH
 
@@ -224,56 +227,68 @@ class WordfenceAPIParser(ParserInterface):
             """ Downsize the score if it's an "Authenticated" vulnerability """
             score = score - 1
 
-        return "Critical" if score == 4 else "High" if score == 3 else "Medium" if score == 2 else "Low"
+        return "Critical" \
+            if score == 4 \
+            else "High" if score == 3 \
+            else "Medium" if score == 2 \
+            else "Low"
 
     def type_is_supported(self, software_type) -> bool:
-        if software_type != "plugin" and software_type != "theme" and software_type != "core":
+        if software_type != "plugin" \
+            and software_type != "theme" \
+                and software_type != "core":
             return False
-    
+
         return True
-    
+
     def get_object_category_tag(self, software_type):
-        return  "wp-theme" if software_type == "theme" else "wp-core" if software_type == "core" else "wp-plugin"
+        return "wp-theme" \
+            if software_type == "theme" \
+            else "wp-core" if software_type == "core" \
+            else "wp-plugin"
 
     def get_template_id(self, cve_id, vuln):
         if cve_id != "":
             logger.debug(f"[ ] CVE ID: {cve_id}")
             return cve_id
-        
+
         unique_id = self.get_uniq_id(vuln.get('id', ''))
 
         object_slug = vuln.get('slug')
 
         logger.debug(f"[ ] No CVE ID. Created new unique ID: {unique_id}")
         return f"{object_slug}-{unique_id}"
-    
+
     def target_version_file(self, software_type, vuln):
         object_slug = vuln.get('slug')
         if software_type == "theme":
             filepath = f"wp-content/themes/{object_slug}/style.css"
         elif software_type == "core":
-            filepath = f"index.php"
+            filepath = "index.php"
         else:
             filepath = f"wp-content/plugins/{object_slug}/readme.txt"
 
         return filepath
-    
+
     def get_version_regex(self, software_type):
         if software_type == "theme":
-            regex = f"(?mi)Version: ([0-9.]+)"
+            regex = "(?mi)Version: ([0-9.]+)"
         elif software_type == "core":
-            regex = f"(?mi)\?v=([0-9.]+)"
+            regex = "(?mi)\?v=([0-9.]+)"
         else:
-            regex = f"(?mi)Stable tag: ([0-9.]+)"
+            regex = "(?mi)Stable tag: ([0-9.]+)"
 
         return regex
 
     def get_uniq_id(self, url: str):
         md5 = hashlib.md5(url.encode())
         return md5.hexdigest()
-    
+
     def get_affected_version(self, affected_version) -> str:
-        """ Turns the affected version string out of the API into nuclei readable format"""
+        """
+        Turns the affected version string out of the API
+        into Nuclei readable format.
+        """
 
         logger.debug(f"[ ] Affected version: {affected_version}")
 
@@ -285,8 +300,18 @@ class WordfenceAPIParser(ParserInterface):
 
         return f"'>= {affected_version['from_version']}', '<= {affected_version['to_version']}'"
 
-    def get_template_filename(self, software_type):
+    def get_template_filename(self, software_type, has_references=True):
+        """
+        Returns the right template for the specific case.
+        Whether it has references or not and depending on software_type.
+        """
+        if has_references is False and software_type == "core":
+            return 'lib/template-wp-core-without-references.yaml'
+
         if software_type == "core":
             return 'lib/template-wp-core.yaml'
 
-        return 'lib/template.yaml'
+        if has_references is False:
+            return 'lib/template-main-without-references.yaml'
+
+        return 'lib/template-main.yaml'
